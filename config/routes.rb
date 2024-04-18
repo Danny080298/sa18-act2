3028d1c7f37d6c1f2c1bc600cbafa5e48b365ab4
Danny Nguyen <dqngyen1@memphis.edu>
2024-04-18 18:53:05 -0500

    Complete SA18-act2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'products#index'
+  resources :products, only: [:index, :show]
+  get 'products/index'
+  get 'products/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name: "Pokemon", price: 20.33, description: "Let Catch them all")
+Product.create(name: "PC", price: 1299.99, description: "Let gaming alright")
+Product.create(name: "Desk", price: 49.99, description: "Are we playing online or offline?")
